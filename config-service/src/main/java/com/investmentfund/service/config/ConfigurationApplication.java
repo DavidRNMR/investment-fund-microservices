06b439f61f70ed91d361a5b2f0e455c897e305fe
David Rivera <uniandrome@hotmail.com>
2023-01-19 11:51:56 +0100

    First commit

---
+++ config-service/src/main/java/com/investmentfund/service/config/ConfigurationApplication.java
@@ -0,0 +1,15 @@
+package com.investmentfund.service.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationApplication {
+
+    public static void main (String [] args){
+        new SpringApplicationBuilder(ConfigurationApplication.class).run(args);
+    }
+}
